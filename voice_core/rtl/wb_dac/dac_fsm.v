d9091b13153d0967be3b075b0a93c66ee02a51a5
Andres Calderon <andres.calderon@admobilize.com>
2020-01-14 22:23:55 -0500

    header updated

--- voice_core/rtl/wb_dac/dac_fsm.v
+++ voice_core/rtl/wb_dac/dac_fsm.v
@@ -1,22 +1,22 @@
 /*
-* Copyright 2016 <Admobilize>
+* Copyright 2016-2020 MATRIX Labs
 * MATRIX Labs  [http://creator.matrix.one]
-* This file is part of MATRIX Creator HDL for Spartan 6
+* 
+* Authors: Kevin Patiño    <kevin.patino@admobilize.com>        
+*
+* This file is part of MATRIX Voice HDL for Spartan 6
 *
 * MATRIX Creator HDL is like free software: you can redistribute
 * it and/or modify it under the terms of the GNU General Public License
 * as published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
-
 * This program is distributed in the hope that it will be useful, but
 * WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * General Public License for more details.
-
 * You should have received a copy of the GNU General Public License along
 * with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 module dac_fsm #(
   parameter DATA_WIDTH = "mandatory",
   parameter SAMP_DAC = "mandatory"
--- voice_core/rtl/wb_mic_array/mic_array.v
+++ voice_core/rtl/wb_mic_array/mic_array.v
@@ -1,22 +1,20 @@
 /*
-* Copyright 2018 <Admobilize>
+* Copyright 2016-2020 MATRIX Labs
 * MATRIX Labs  [http://creator.matrix.one]
-* This file is part of MATRIX Voice HDL for Spartan 6
 *
-* Authors:
-*    andres.calderon@admobilize.com
-*    kevin.patino@admobilize.com
+* Authors: Andres Calderon <andres.calderon@admobilize.com>
+*          Kevin Patiño    <kevin.patino@admobilize.com>        
+*
+* This file is part of MATRIX Voice HDL for Spartan 6
 *
 * MATRIX Creator HDL is like free software: you can redistribute
 * it and/or modify it under the terms of the GNU General Public License
 * as published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
-
 * This program is distributed in the hope that it will be useful, but
 * WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * General Public License for more details.
-
 * You should have received a copy of the GNU General Public License along
 * with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
